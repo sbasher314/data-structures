1570572f85e57e4573738448a99f2eb7fae3c602
Frederic Rosselet <fred.rosselet@gmail.com>
2021-01-09 11:10:09 -0800

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    list.tail = newNode;
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.head.next = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var refHead = list.head;
+    delete list.head;
+    list.head = refHead.next;
+    return refHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -17,13 +36,14 @@ var LinkedList = function() {
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * .addToTail: O(1) (constant)
+ * .removeHead: O(1) (constant)
+ * .contains: O(n) (linear)
  */
