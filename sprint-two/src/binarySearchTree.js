a7089518fe3b6925df564f97839127499ae0a172
Frederic Rosselet <fred.rosselet@gmail.com>
2021-01-09 11:14:39 -0800

    Complete Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,56 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(methods);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
 };
 
+var methods = {};
+
+methods.insert = function(value) {
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+methods.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  }
+  if (value < this.value && this.left !== null) {
+    return this.left.contains(value);
+  }
+  if (value > this.value && this.right !== null) {
+    return this.right.contains(value);
+  }
+  return false;
+};
+
+methods.depthFirstLog = function(callback) {
+  callback(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .insert(): O(log n), at worst O(n)
+ .contains(): O(log n), at worst O(n)
+ .depthFirstLog: O(log n), at worst O(n)
  */
